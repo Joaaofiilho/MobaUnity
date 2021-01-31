29b4861c767f21f50edd636a0d2975bfa6a922a6
João Ferreira <joaot10112@gmail.com>
2021-01-31 19:34:41 -0300

    Escala de todo o projeto ajustada; Implementada movimentação do player
    Um novo mapa adicionado utilizando ProBuilder

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,24 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    public void Move(InputAction.CallbackContext context)
-    {
-        Vector2 direction = context.ReadValue<Vector2>();
-        Debug.Log(direction);
-    }
+    [SerializeField]
+    private LayerMask whatIsGround;
 
-    // Start is called before the first frame update
+    [SerializeField]
+    private Camera playerCamera;
+
+    private NavMeshAgent navMeshAgent;
+
+    private Vector3 destination;
+
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(destination != null)
+            Debug.DrawRay(transform.position, destination.normalized * 2);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        //Obtendo o raio apontando da posição da câmera até o ponto da posição do clique do mouse
+        Ray ray = playerCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100, whatIsGround))
+        {
+            navMeshAgent.destination = hit.point;
+            destination = hit.point;
+        }
     }
 }
