a5527ef16ed1e2c99a313f318be19456c9773b22
João Ferreira <joaot10112@gmail.com>
2021-01-31 02:02:44 -0300

    Adicionado mapa com ProBuilder; Adicionado Player; Sistema de câmeras

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public void Move(InputAction.CallbackContext context)
+    {
+        Vector2 direction = context.ReadValue<Vector2>();
+        Debug.Log(direction);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
